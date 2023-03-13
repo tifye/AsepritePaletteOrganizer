96b442086954538ef04ab482304a3a2e54f81a9c
tifufu <dematasjoshua@hotmail.com>
2023-03-13 22:05:22 +0100

    Created classes for PaletteGroup and PaletteGroupCollection for easier management. Started implementing MVC pattern for better and more clean approach to ui

--- Palette Organizer.lua
+++ Palette Organizer.lua
@@ -1,32 +1,4 @@
--- --------------------------- Main Dialog ---------------------------
-local CreateMainDialog = function ()
-    local dialog = Dialog {
-        title = "Palette Organizer"
-    }
-
-    dialog.bounds = Rectangle(250, dialog.bounds.y + 250, 250, 100)
-
-    dialog
-    :button {
-        text="Add Group",
-        onclick=function ()
-            -- AddPaletteGroup(MainDialog)
-        end
-    }
-    :button {
-        text="Edit Groups",
-        onclick=function ()
-            -- EditPaletteGroups(MainDialog)
-        end
-    }
-    :separator {
-        text = "Groups"
-    }
-    return dialog
-end 
--- --------------------------- Main Dialog --------------------------
--- ------------------------------------------------------------------
--- --------------------------- Palette Group ------------------------
+-- --------------------------- Palette Group -------------------------
 PaletteGroup = {}
 PaletteGroup.__index = PaletteGroup
 
@@ -37,9 +9,109 @@ function PaletteGroup:new(id, label, colors)
     self.colors = colors
     return self
 end
--- --------------------------- Palette Group ------------------------
+-- --------------------------- Palette Group -------------------------
 
-local MainDialog = CreateMainDialog()
-MainDialog:show {
-    wait=false
-}
+-- --------------------------- Palette Group Collection --------------
+PaletteGroupCollection = {}
+PaletteGroupCollection.__index = PaletteGroupCollection
+
+function PaletteGroupCollection:new()
+    local self = setmetatable({}, PaletteGroupCollection)
+    self.groups = {}
+    return self
+end
+
+function PaletteGroupCollection:Add(paletteGroup)
+    table.insert(self.groups, paletteGroup)
+end
+
+function PaletteGroupCollection:TryGet(id)
+    local groudIndex = self:IndexOf(id)
+    if groudIndex > -1 then
+        return self.groups[groudIndex]
+    end
+    return nil
+end
+
+function PaletteGroupCollection:Remove(id)
+    local groudIndex = self:IndexOf(id)
+    if groudIndex > -1 then
+        table.remove(self.groups, groudIndex)
+    end
+end
+
+function PaletteGroupCollection:IndexOf(id)
+    for i, group in ipairs(self.groups) do
+        if group.id == id then
+            return i
+        end
+    end
+    return -1
+end
+-- --------------------------- Palette Group Collection --------------
+
+-- --------------------------- Main Dialog ---------------------------
+local CreateMainDialog = function (controller)
+    local dialog = Dialog {
+        title = "Palette Organizer"
+    }
+
+    dialog.bounds = Rectangle(250, dialog.bounds.y + 250, 250, 100)
+
+    dialog
+    :button {
+        text="Add Group",
+        onclick=function ()
+            controller:ShowAddPaletteGroupDialog()
+        end
+    }
+    :button {
+        text="Edit Groups",
+        onclick=function ()
+            controller:ShowEditPaletteGroupDialog()
+        end
+    }
+    :separator {
+        text = "Groups"
+    }
+
+    controller.mainDialog = dialog
+    return dialog
+end 
+-- --------------------------- Main Dialog --------------------------
+
+-- --------------------------- Helper Functions ---------------------
+local AttachPaletteGroupEditControls = function (dialog)
+    
+end
+-- --------------------------- Helper Functions ---------------------
+
+-- --------------------------- Controller ---------------------------
+Controller = {}
+Controller.__index = Controller
+
+function Controller:new()
+    local self = setmetatable({}, Controller)
+    self.paletteGroups = PaletteGroupCollection:new()
+    return self
+end
+
+function Controller:Start()
+    local MainDialog = CreateMainDialog(self)
+    MainDialog:show {
+        wait=false
+    }
+end
+
+function Controller:ShowAddPaletteGroupDialog()
+    print("ShowAddPaletteGroupDialog")
+end
+
+function Controller:ShowEditPaletteGroupDialog()
+    print("ShowEditPaletteGroupDialog")
+end
+-- --------------------------- Controller ---------------------------
+
+
+local controller = Controller:new()
+controller:Start()
