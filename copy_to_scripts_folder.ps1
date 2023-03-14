c1c52c9308a172b81324866fa1f0e9181c984380
tifye <dematasjoshua@hotmail.com>
2023-03-14 01:36:02 +0100

    Delete copy_to_scripts_folder.ps1

--- copy_to_scripts_folder.ps1
+++
@@ -1,2 +0,0 @@
-Remove-Item "C:\Users\demat\AppData\Roaming\Aseprite\scripts\Palette Organizer.lua"
-Copy-Item ".\Palette Organizer.lua" -Destination "C:\Users\demat\AppData\Roaming\Aseprite\scripts"
